b820869d1abf7db13d622aefe96bbfde505e07c4
Yash Goyal <yashgo0018@gmail.com>
2019-03-26 17:42:33 +0530

    Project Getting Shape

--- blog/settings.py
+++ blog/settings.py
@@ -8,7 +8,7 @@ TITLE = "Opium Blog"
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6jik_)q3eysljjobq!+4f&jo4gg6zl#et8tqq-aysmxs_1*h63'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -143,8 +143,8 @@ CKEDITOR_CONFIGS = {
 
 # Config for email server
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'y@ash123'
-EMAIL_HOST_USER = 'yashgo0018@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
